66b95c6cda6aae8bc75e52b410fd7310e330552b
Leanstix <leanstixx@gmail.com>
2024-01-04 02:46:04 +0100

    test

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/bin/bash/python
 str = "python is an interpreted, interactive, object-oriented programming\
-        language that combines remarkable power with very clear syntax"
+ language that combines remarkable power with very clear syntax"
 str = str[39:66] + str[106:112] + str[:6]
 print(str)
