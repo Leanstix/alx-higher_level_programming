854bbd645a46a8d492d2c09be717b66f1a4333b0
Leanstix <leanstixx@gmail.com>
2024-06-05 07:34:51 +0100

    done

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+
 const fs = require('fs');
-const process = require('process');
-const filePath = process?.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(filePath, 'utf-8', (error, data) => {
-  if (error) return console.log(error);
-
-  console.log(data);
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
 });
